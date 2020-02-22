1963956b2588ceda66ac5b1f0e05470179cdf136
derek <senpaiguru1488@gmail.com>
2020-02-22 13:01:06 +0300

    First version

---
+++ main.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'telegram/bot'
+
+TOKEN = ''
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "I see you wanna set a song lyric to your bio, #{message.from.first_name}")
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    end
+  end
+end
